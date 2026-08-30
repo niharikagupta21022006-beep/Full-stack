2a5807c8f911b79dbf3da9858fa0685c43a08bba
niharikagupta21022006-beep <niharikagupta21022006@gmail.com>
2026-08-31 00:37:05 +0530

    docker

--- tradingcore/tradingcore/settings.py
+++ tradingcore/tradingcore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,14 +81,7 @@ WSGI_APPLICATION = 'tradingcore.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'tradingcore_db',
-        'USER':'postgres',
-        'PASSWORD':'admin123',
-        'HOST':'host.docker.internal',
-        'PORT':'5432',
-    }
+    'default': dj_database_url.config(default='postgress://localhost/tradingcore')
 }
 
 
